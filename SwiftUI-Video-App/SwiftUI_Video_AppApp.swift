c95906c8354074c1aa2198c01ffe0e034bf1e4da
Yigit Esmen <yigit.esmen@gmail.com>
2022-07-06 16:42:44 +0300

    Initial Commit

---
+++ SwiftUI-Video-App/SwiftUI_Video_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Video_AppApp.swift
+//  SwiftUI-Video-App
+//
+//  Created by Yigit Esmen on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Video_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            VideoListView()
+        }
+    }
+}
---
+++ SwiftUI-Video-App/View/VideoDetailView.swift
@@ -0,0 +1,68 @@
+//
+//  VideoDetailView.swift
+//  SwiftUI-Video-App
+//
+//  Created by Yigit Esmen on 7/6/22.
+//
+
+import SwiftUI
+
+struct VideoDetailView: View {
+    var video: Video
+    var body: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            
+            Image(video.imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 150)
+                .cornerRadius(12)
+            
+            Text(video.title)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .lineLimit(2)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+            
+            HStack(spacing: 40) {
+                Label("\(video.viewCount)", systemImage: "eye.fill")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                Text(video.uploadDate)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+            
+            Text(video.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Link(destination: video.url, label: {
+                StandardButton(title: "Watch Now")
+            })
+        }
+    }
+}
+
+struct StandardButton: View {
+    var title: String
+    var body: some View {
+        Text(title)
+            .bold()
+            .font(.title2)
+            .frame(width: 280, height: 50)
+            .background(Color(.systemRed))
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+}
+
+struct VideoDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoDetailView(video: VideoList.topTen.first!)
+    }
+}
